5777cf70819455dfb4fb4511d2bb0295db8d9960
Reem H <reemhamoui92@gmail.com>
2020-04-06 16:19:14 -0400

    why did i choose this idea

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ app.getInfo = function () {
                     const landscapeArray = data.results.filter(landscapeImages => {
                         return landscapeImages.height < 3400
                     })
-                    
+
                     imageGenerator = () => {
                         const randomizeImage = landscapeArray[Math.floor(Math.random() * landscapeArray.length)]
-                        
+
                         const imageURL = randomizeImage.urls.regular
                         const imageHeight = randomizeImage.height;
                         document.getElementById('cafeImage').innerHTML = `
@@ -41,12 +41,45 @@ app.getInfo = function () {
         apiCall();
     }
 
-    musicCall = () =>{
-        const audio = new Audio(`https://www.youtube.com/watch?v=5qap5aO4i9A`)
+    musicCall = () => {
 
+
+        const audio = new Audio(`https://www.youtube.com/watch?v=5qap5aO4i9A`)
         audio.play();
 
+        function onYouTubeIframeAPIReady() {
+            var e = document.getElementById("youtube-audio"),
+                t = document.createElement("img");
+            t.setAttribute("id", "youtube-icon"), t.style.cssText = "cursor:pointer;cursor:hand", e.appendChild(t);
+            var a = document.createElement("div");
+            a.setAttribute("id", "youtube-player"), e.appendChild(a);
+            var o = function (e) {
+                var a = e ? "hFuieHT" : "hFuieHT.png";
+                t.setAttribute("src", "https://i.imgur.com/" + a)
+            };
+            e.onclick = function () {
+                r.getPlayerState() === YT.PlayerState.PLAYING || r.getPlayerState() === YT.PlayerState.BUFFERING ? (r.pauseVideo(), o(!1)) : (r.playVideo(), o(!0))
+            };
+            var r = new YT.Player("youtube-player", {
+                height: "0",
+                width: "0",
+                videoId: e.dataset.video,
+                playerVars: {
+                    autoplay: e.dataset.autoplay,
+                    loop: e.dataset.loop
+                },
+                events: {
+                    onReady: function (e) {
+                        r.setPlaybackQuality("small"), o(r.getPlayerState() !== YT.PlayerState.CUED)
+                    },
+                    onStateChange: function (e) {
+                        e.data === YT.PlayerState.ENDED && o(!1)
+                    }
+                }
+            })
+        }
 
+        onYouTubeIframeAPIReady();
     }
     console.log('haha')
     unsplashCall();
